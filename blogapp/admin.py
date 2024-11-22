480ebbe775bc3a11fecd28d5a1c63782602b323d
kacem <kacemmeherzi52@gmail.com>
2024-11-22 11:31:48 +0100

    blogpost and author is registered to admin panel

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from blogapp.models import Author, BlogPost
+
 # Register your models here.
+admin.site.register(BlogPost)
+admin.site.register(Author)
+
+
+
+
+
+
+
+
+
+
